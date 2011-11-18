291fadd6c1904115e19b9ed23c9f7637de450725
Anton Fedchenko <kompotik@gmail.com>
2011-11-18 23:53:47 +0400

    Extracted AbstractServlet class to make component easier to adopt. Closes #10.

--- src/main/java/com/gmail/kompotik/gwtupld/server/UploadServlet.java
+++ src/main/java/com/gmail/kompotik/gwtupld/server/UploadServlet.java
@@ -1,29 +1,21 @@
 package com.gmail.kompotik.gwtupld.server;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
 
-public class UploadServlet extends HttpServlet {
+public class UploadServlet extends AbstractGwtupldServlet {
   private String urlPath;
   private String realPath;
 
@@ -35,123 +27,56 @@ public class UploadServlet extends HttpServlet {
   }
 
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    setAllowedHeaders(resp);
+  protected UploadedFile saveMultipartFile(FileItem item,
+                                           HttpServletRequest req,
+                                           List<UploadedFile> files)
+      throws Exception {
+    String albumId = getAlbumId(req);
 
-    resp.getWriter().write("please do `POST` request");
+    final String filename = URLDecoder.decode(item.getName(), "UTF-8");
+    File file = new File(realPath + albumId + filename);
+    file.getParentFile().mkdirs();
+    item.write(file);
+    return new UploadedFile(
+        filename,
+        (int) item.getSize(),
+        item.getContentType(),
+        urlPath + albumId + filename
+    );
   }
 
-  private void setAllowedHeaders(HttpServletResponse resp) {
-    resp.setHeader("Access-Control-Allow-Origin", "*");
-    resp.setHeader("Access-Control-Allow-Headers", "X-File-Name,X-File-Type," +
-        "X-File-Size,X-Requested-With,Content-Type");
+  @Override
+  protected UploadedFile saveXhrFile(InputStream is,
+                                     FileOutputStream fos,
+                                     HttpServletRequest req,
+                                     List<UploadedFile> files)
+      throws IOException {
+    String albumId = getAlbumId(req);
+
+    String filename = URLDecoder.decode(req.getHeader("X-File-Name"), "UTF-8");
+    final File file = new File(realPath + albumId + filename);
+    file.getParentFile().mkdirs();
+    fos = new FileOutputStream(file);
+    IOUtils.copy(is, fos);
+
+    //      uploadedFile.type = item.getContentType();
+    //      uploadedFile.url = file.getPath();
+
+    return new UploadedFile(
+        filename,
+        (int) file.length(),
+        null,
+        urlPath + albumId + filename
+    );
   }
 
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    setAllowedHeaders(resp);
-    resp.setCharacterEncoding("UTF-8");
-    resp.setHeader("Content-Type", "application/json");
-
+  private String getAlbumId(HttpServletRequest req)
+      throws UnsupportedEncodingException {
     String albumId = "";
     if (req.getParameter("albumId") != null) {
       // testing paramaters passed on submit
       albumId = URLDecoder.decode(req.getParameter("albumId"), "UTF-8") + "/";
     }
-
-    final PrintWriter writer = resp.getWriter();
-    if (req.getContentType().equals("application/octet-stream")) {
-      List<UploadedFile> files = new ArrayList<UploadedFile>();
-      saveOctetStream(req, resp, files, albumId);
-      writer.write(new Gson().toJson(files));
-      writer.flush();
-      writer.close();
-      return;
-    }
-
-    if (!ServletFileUpload.isMultipartContent(req)) {
-      throw new IllegalArgumentException("Request is not multipart," +
-          "please 'multipart/form-data' enctype for your form.");
-    }
-
-    ServletFileUpload sfu = new ServletFileUpload(new DiskFileItemFactory());
-    try {
-      List<FileItem> items = sfu.parseRequest(req);
-      List<UploadedFile> files = new ArrayList<UploadedFile>();
-      for (FileItem item : items) {
-        if (!item.isFormField()) {
-          final String filename = URLDecoder.decode(item.getName(), "UTF-8");
-          File file = new File(realPath + albumId + filename);
-          file.getParentFile().mkdirs();
-          item.write(file);
-          final UploadedFile uploadedFile = new UploadedFile(
-              filename,
-              (int) item.getSize(),
-              item.getContentType(),
-              urlPath + albumId + filename
-          );
-          files.add(uploadedFile);
-        }
-      }
-      writer.write(new Gson().toJson(files));
-    } catch (FileUploadException e) {
-      throw new RuntimeException(e);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    } finally {
-      writer.flush();
-      writer.close();
-    }
-  }
-
-  @Override
-  protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    setAllowedHeaders(resp);
-  }
-
-  private void saveOctetStream(HttpServletRequest request,
-                               HttpServletResponse response,
-                               List<UploadedFile> files,
-                               String albumId) {
-    InputStream is = null;
-    FileOutputStream fos = null;
-
-    try {
-      String filename = URLDecoder.decode(request.getHeader("X-File-Name"), "UTF-8");
-      is = request.getInputStream();
-      final File file = new File(realPath + albumId + filename);
-      file.getParentFile().mkdirs();
-      fos = new FileOutputStream(file);
-      IOUtils.copy(is, fos);
-      final UploadedFile uploadedFile = new UploadedFile(
-          filename,
-          (int) file.length(),
-          null,
-          urlPath + albumId + filename
-      );
-//      uploadedFile.type = item.getContentType();
-//      uploadedFile.url = file.getPath();
-      files.add(uploadedFile);
-      response.setStatus(HttpServletResponse.SC_OK);
-    } catch (FileNotFoundException ex) {
-      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      System.out.println(UploadServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
-      log(UploadServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
-    } catch (IOException ex) {
-      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      System.out.println(UploadServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
-      log(UploadServlet.class.getName() + "has thrown an exception: " + ex.getMessage());
-    } finally {
-      IOUtils.closeQuietly(fos);
-      IOUtils.closeQuietly(is);
-    }
-  }
-
-  @Override
-  protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    super.doDelete(req, resp);
+    return albumId;
   }
 }
